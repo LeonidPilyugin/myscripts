fc4b7f71d9f2e85a957789a9d052cddfc197caf3
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-07-26 13:28:21 +0300

    saveconda

--- saveconda.sh
+++ saveconda.sh
@@ -5,5 +5,5 @@ DIRECTORY=~/conda-dumps
 mkdir -p $DIRECTORY
 
 for env in $(conda env list | tail +3 | cut -d' ' -f2- | xargs); do
-    conda env export -n "$env" > "$DIRECTORY/${env}.yaml"
+    conda env export -n "$env" > "$DIRECTORY/$(basename ${env}).yaml"
 done
