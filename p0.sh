e92b650f5e5de5e619a9539b9f5acca947eff884
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-07-27 19:12:17 +0300

    p0

---
+++ p0.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+file=/tmp/$(hexdump -vn16 -e'4/4 "%08x" 1 "\n"' /dev/urandom).toml
+cat <<EOF > "$file"
+[task]
+id = "p0.$1"
+comment = "Compute zero isobar"
+
+[task.backend]
+type = "screen"
+
+[task.wrapper]
+type = "default"
+command = "zsh -c 'source $HOME/.zshrc && conda activate p0 && p0.py $HOME/descriptors/p0/$1.toml'"
+mixed_stdout = false
+EOF
+
+conda activate spmi
+python3 "$HOME/opt/spmi/src/spmi/app.py" "\'p0.$1\'"
+conda deactivate
+
+rm "$file"
