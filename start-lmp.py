107973274140bb56e8eb28ae15b038726b0f0404
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-08-03 18:09:16 +0300

    start-lmp

--- start-lmp.py
+++ start-lmp.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
         with open(filename, "w") as f:
             toml.dump(settings, f)
         
-        assert os.system(f"zsh -c 'source ~/.zshrc && spmi load {filename} && spmi start simulation.{data['id']}'") == 0
+        assert os.system(f"zsh -c \'source ~/.zshrc && spmi load {filename} && spmi start simulation.{data['id']}\'") == 0
     finally:
         os.unlink(filename)
 
