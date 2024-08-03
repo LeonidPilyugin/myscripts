a319f402cfb64ebe3f27b45a16c7d333d151655f
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-08-03 17:07:48 +0300

    start-lmp

--- start-lmp.py
+++ start-lmp.py
@@ -47,11 +47,9 @@ if __name__ == "__main__":
         with open(filename, "w") as f:
             toml.dump(settings, f)
         
-        with open(filename) as f:
-            print(f.read())
-
         assert os.system(f"spmi load {filename}") == 0
         assert os.system(f"spmi start {data['id']}") == 0
     finally:
-        os.unlink(filename)
+        # os.unlink(filename)
+        pass
 
