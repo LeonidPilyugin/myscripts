fddc9ddb1e2b5aa02608cd4246b0c261094b72c9
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-08-24 12:14:42 +0300

    openmm-addons

--- openmm-addons/uo2-add-xe.py
+++ openmm-addons/uo2-add-xe.py
@@ -79,6 +79,6 @@ def main(step, simulation, data):
         vx=Aml.DoublePerAtomProperty.from_array(vx),
         vy=Aml.DoublePerAtomProperty.from_array(vy),
         vz=Aml.DoublePerAtomProperty.from_array(vz),
-        mass=Aml.DoublePerAtomProperty.from_array(masses),
+        mass=Aml.DoublePerAtomProperty.from_array([m.value_in_unit(unit.atom_mass_units) for m in masses]),
         type=Aml.IntPerAtomProperty.from_array(types),
     )
