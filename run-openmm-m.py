b05c39715a9aa1fc0ff6490db4420ee31d0ab49f
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-09-05 19:03:18 +0300

    minrep

--- run-openmm-m.py
+++ run-openmm-m.py
@@ -288,6 +288,7 @@ if __name__ == "__main__":
             while st != step:
                 lines.append(line)
                 line = f.readline()
+                print(line)
                 st = int(line.split(",")[0])
 
         with open(thermo_file, "w") as f:
