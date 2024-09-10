8773e8164259e19a9665be86b1b491230acf4b84
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-09-10 08:07:07 +0300

    minrep

--- openmm-addons/uo2-add-xe.py
+++ openmm-addons/uo2-add-xe.py
@@ -36,6 +36,8 @@ def main(step, simulation, data):
     # for p in nearest:
         # system.setParticleMass(p, masses[p])
 
+    print(com[0], positions[0][3])
+
     nearest = sorted(
         list(range(n)),
         key = lambda i: sum([positions[i][j] - com[j].value_in_unit(unit.nanometer) for j in range(3)])
