18a695978bd61dd301eb96524541f09c9e644abf
Leonid Pilyugin <l.pilyugin04@gmail.com>
2025-01-26 17:25:55 +0300

    columns

--- src/openmm/addon.py
+++ src/openmm/addon.py
@@ -11,7 +11,7 @@ class Unit:
         self.velocity = self.length / self.time
         self.energy = openmm.unit.kilojoule_per_mole
         self.mass = openmm.unit.atom_mass_units
-        self.force = self.energy / openmm.unit.length
+        self.force = self.energy / self.length
 
 
 class Logger(AmlCore.DataObject):
