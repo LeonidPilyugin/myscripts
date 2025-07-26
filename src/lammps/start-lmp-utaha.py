ba292152d923895c3aa30f0de316db477af02d3a
Leonid Pilyugin <l.pilyugin04@gmail.com>
2025-07-26 13:52:12 +0300

    utaha

--- src/lammps/start-lmp-utaha.py
+++ src/lammps/start-lmp-utaha.py
@@ -54,6 +54,8 @@ if __name__ == "__main__":
     try:
         with open(filename, "w") as f:
             json.dump(descriptor, f)
-        assert os.system(f"zsh -c 'source ~/.zshrc && spmi load {filename} && spmi start simulation.{data['id']}'") == 0
+        assert os.system(f"zsh -c 'source ~/.zshrc && utaha --load {filename} && utaha --start simulation.{data['id']}'") == 0
+        assert(os.system(f"utaha --load {filename}"))
+        assert(os.system(f"utaha --start --alias 'simulation.{data['id']}'"))
     finally:
         os.unlink(filename)
